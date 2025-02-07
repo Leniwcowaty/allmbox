f56a18ed6f69699821f704fb78b5f95c7d7608fa
leniwcowaty <krystian.kacprzak.98@gmail.com>
2025-02-07 22:30:53 +0100

    Added ability to install different ollama models and create multiple instances

--- build/setup_env.sh
+++ build/setup_env.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-model="llama3.2"
-
 curl -fsSL https://ollama.com/install.sh | sh
 sleep 3
 sudo /usr/bin/systemctl start ollama.service
 sleep 3
-echo "/bye" | /usr/local/bin/ollama run $model
+echo "/bye" | /usr/local/bin/ollama run $1
 curl -fsSL https://cdn.anythingllm.com/latest/installer.sh | sh
 
 echo "@reboot root /usr/bin/systemctl start ollama.service && /usr/share/ollama run llama:3.2" | sudo tee -a /etc/crontab
